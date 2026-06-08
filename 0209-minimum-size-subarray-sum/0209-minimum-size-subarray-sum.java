59ed6980f0bd3cd4f2069965a901a9fcd3f99f3a
Harsh Bharti <harshbharti179@gmail.com>
2026-06-08 21:55:33 +0530

    Time: 1 ms (99.96%), Space: 62.1 MB (48.82%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
@@ -2,22 +2,22 @@ class Solution {
     public int minSubArrayLen(int target, int[] nums) {
         int low = 0;
         int high = 0;
-        int sum = 0;
+        int currentSum = 0;
         // Stores minimum valid window length found
-        int res = Integer.MAX_VALUE;
+        int minLenWindow = Integer.MAX_VALUE;
 
         // Expand window using high pointer
         while (high < nums.length) {
-            sum = sum + nums[high];
-            while (sum >= target) {
-                int len = high - low + 1;
-                res = Math.min(res, len);
-                sum = sum - nums[low];
+            currentSum = currentSum + nums[high];
+            while (currentSum >= target) {
+                int currentWindowSize = high - low + 1;
+                minLenWindow = Math.min(minLenWindow, currentWindowSize);
+                currentSum = currentSum - nums[low];
                 low++;
             }
             high++;
         }
         // If no valid subarray exists, return 0
-        return res == Integer.MAX_VALUE ? 0 : res;
+        return minLenWindow == Integer.MAX_VALUE ? 0 : minLenWindow;
     }
 }
